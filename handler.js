ebe07f8ba9a3c77032ad5c24c4db11f38bef19e3
ssmith2 <ssmith2@asurion.com>
2017-03-11 09:43:45 -0600

    test success

--- handler.js
+++ handler.js
@@ -3,10 +3,15 @@
 const config = require('./lib/config.js');
 const Alexa = require('alexa-sdk');
 const https = require('https');
-const alexa = "";
 var welcomeMessage = " Nashville Neighbors. You can ask me for public community resource information,  or  say help. What will it be?";
 
 var welcomeReprompt = "You can ask me for a community resource category, or  say help. What will it be?";
+
+var HelpMessage = "Here are some things you  can say: Give me community resource information. Tell me about Nashville Neighbors.  What would you like to do?";
+
+var goodbyeMessage = "OK, thanks for being a Nashville Neighbor.";
+
+var output = "";
 
 var newSessionHandlers = {
     'LaunchRequest': function() {
@@ -14,11 +19,22 @@ var newSessionHandlers = {
         output = welcomeMessage;
 
         this.emit(':ask', output, welcomeReprompt);
+    },
+    'Unhandled': function() {
+        output = HelpMessage;
+        this.emit(':ask', output, welcomeReprompt);
+    },
+    'AMAZON.StopIntent': function() {
+        this.emit(':tell', goodbyeMessage);
+    },
+    'SessionEndedRequest': function() {
+        // Use this function to clear up and save any data needed between sessions
+        this.emit('AMAZON.StopIntent');
     }
 };
 
 module.exports.index = (event, context, callback) => {
-    alexa = Alexa.handler(event, context);
+    var alexa = Alexa.handler(event, context);
     alexa.registerHandlers(newSessionHandlers);
     alexa.execute();
 };
