e5e7a2b8761d3ebf1829f1133860bfde9acc5b6d
ssmith2 <ssmith2@asurion.com>
2017-03-11 12:57:52 -0600

    remove state

--- handler.js
+++ handler.js
@@ -29,25 +29,48 @@ var tryAgainMessage = "please try again."
 
 var output = "";
 
-var states = {
-    SEARCHMODE: '_SEARCHMODE',
-    CATEGORIES: '_CATEGORIES',
-    NEIGHBOR: '_NEIGHBOR'
-};
-
 var newSessionHandlers = {
     'LaunchRequest': function() {
         output = welcomeMessage;
         this.emit(':ask', output, welcomeReprompt);
     },
     'getOverview': function() {
-        this.handler.state = states.SEARCHMODE;
-        this.emitWithState('getOverview');
+      output = welcomeMessage;
+      this.emit(':ask', output, welcomeReprompt);
     },
 
     'getCompleteCategoryListIntent': function() {
-        this.handler.state = states.SEARCHMODE;
-        this.emitWithState('getCompleteCategoryListIntent');
+      var context = this;
+      https.get(config.communityDataHost, config.communityDataPath, function(err, data) {
+          if (err) {
+              console.error(err);
+              context.emit(':tell', noCategoryErrorMessage, welcomeReprompt);
+              return;
+          }
+
+          var excludeArr = [];
+
+          data = JSON.parse(data);
+
+          var finalArr = data.filter(function(element) {
+              if (excludeArr.indexOf(element.contact_type) !== -1 || config.excludedCategories.indexOf(element.contact_type.toLowerCase()) !== -1) {
+                  return false;
+              } else {
+                  excludeArr.push(element.contact_type);
+                  return true;
+              }
+          });
+
+          var categories = "";
+          finalArr.forEach(function(contact) {
+              categories = contact.contact_type + ", " + categories;
+          });
+
+          categories = utils.contentCleanUp(categories);
+
+          output = "Here is a list of community resources available in Nashville, " + categories;
+          context.emit(':tell', output, getMoreInfoRepromptMessage);
+      });
     },
 
     'Unhandled': function() {
@@ -63,80 +86,8 @@ var newSessionHandlers = {
     }
 };
 
-var startSearchHandlers = Alexa.CreateStateHandler(states.SEARCHMODE, {
-    'AMAZON.HelpIntent': function() {
-        output = HelpMessage;
-        this.emit(':ask', output, HelpMessage);
-    },
-
-    'getOverview': function() {
-        output = welcomeMessage;
-        this.emit(':ask', output, welcomeReprompt);
-    },
-
-    'getCompleteCategoryListIntent': function() {
-        var context = this;
-        https.get(config.communityDataHost, config.communityDataPath, function(err, data) {
-            if (err) {
-                console.error(err);
-                context.emit(':tell', noCategoryErrorMessage, welcomeReprompt);
-                return;
-            }
-
-            var excludeArr = [];
-
-            data = JSON.parse(data);
-
-            var finalArr = data.filter(function(element) {
-                if (excludeArr.indexOf(element.contact_type) !== -1 || config.excludedCategories.indexOf(element.contact_type.toLowerCase()) !== -1) {
-                    return false;
-                } else {
-                    excludeArr.push(element.contact_type);
-                    return true;
-                }
-            });
-
-            var categories = "";
-            finalArr.forEach(function(contact) {
-                categories = contact.contact_type + ", " + categories;
-            });
-
-            categories = utils.contentCleanUp(categories);
-
-            output = "Here is a list of community resources available in Nashville, " + categories;
-            context.emit(':tell', output, getMoreInfoRepromptMessage);
-        });
-    },
-
-    'AMAZON.YesIntent': function() {
-        output = HelpMessage;
-        this.emit(':ask', output, HelpMessage);
-    },
-
-    'AMAZON.NoIntent': function() {
-        output = HelpMessage;
-        this.emit(':ask', HelpMessage, HelpMessage);
-    },
-
-    'AMAZON.StopIntent': function() {
-        this.emit(':tell', goodbyeMessage);
-    },
-
-    'AMAZON.RepeatIntent': function() {
-        this.emit(':ask', output, HelpMessage);
-    },
-    'SessionEndedRequest': function() {
-        // Use this function to clear up and save any data needed between sessions
-        this.emit('AMAZON.StopIntent');
-    },
-    'Unhandled': function() {
-        output = HelpMessage;
-        this.emit(':ask', output, welcomeReprompt);
-    }
-});
-
 module.exports.index = (event, context, callback) => {
     var alexa = Alexa.handler(event, context);
-    alexa.registerHandlers(newSessionHandlers, startSearchHandlers);
+    alexa.registerHandlers(newSessionHandlers);
     alexa.execute();
 };
